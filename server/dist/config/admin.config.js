ddb42641765fdce574441db1ec2abe1f9348ab96
chunnuthakur <ck.8107@gmail.com>
2026-05-07 09:56:32 +0530

    chore: optimize .gitignore and untrack sensitive/build files

--- server/dist/config/admin.config.js
+++
@@ -1,7 +0,0 @@
-// Default admin credentials
-// IMPORTANT: Change these values before deploying to production!
-export const DEFAULT_ADMIN = {
-    email: 'prabhat@autoformindia.com',
-    password: 'Admin@123',
-    name: 'Admin User'
-};
